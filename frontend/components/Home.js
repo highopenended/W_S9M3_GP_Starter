c0f7c4d7f874d0371829728ea1968f337e10c7ba
Matt Glenn <highopenended@gmail.com>
2024-08-23 17:18:43 -0400

    Final cleanup

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,32 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import {useAuth} from '../hooks/index.js'
+import { useAuth } from '../hooks/index.js'
 
 export default function Home() {
   const navigate = useNavigate()
   const {auth, checkAuth}=useAuth(()=>navigate('/login'))
-
-  // const [auth, setAuth] = useState(null)
-  // async function checkAuth() {
-  //   try {
-  //     await axios.get(
-  //       `${BASE_URL}/is_authed`,
-  //       { headers: { Authorization: localStorage.getItem('token') } },
-  //     )
-  //     setAuth(true)
-  //   } catch (err) {
-  //     localStorage.removeItem('token')
-  //     setAuth(false)
-  //   }
-  // }
-  // useEffect(() => {
-  //   if (!localStorage.getItem('token')) navigate('/login')
-  //   else checkAuth()
-  // }, [])
-  // useEffect(() => {
-  //   if (auth === false) navigate('/login')
-  // }, [auth])
-
   const sensitiveStuff = async () => {
     await checkAuth()
     // doing sensitive stuff
